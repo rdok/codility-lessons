b39813b88975c81e761503feed3752ecd4e1289c
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-26 14:14:43 +0000

    Find equilibrium given an index.

--- app/Equilibrium.php
+++ app/Equilibrium.php
@@ -11,5 +11,66 @@ namespace App;
  */
 class Equilibrium
 {
+    /**
+     * @var array
+     */
+    private $data;
 
+    /**
+     * Equilibrium constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return array|int
+     */
+    public function getIndices()
+    {
+        $indices = [];
+
+        $dataSize = count($this->data);
+
+        for ($index = 0; $index < $dataSize; ++$index) {
+            if ($this->isEquilibriumIndexOfLeftPart($index, $this->data[ $index ])) {
+                $indices[] = $this->data[ $index ];
+            }
+        }
+
+        return 0 === count($indices) ? $this->noIndices() : $indices;
+    }
+
+    /**
+     * @param $equilibriumIndex
+     * @return bool
+     */
+    public function isEquilibriumIndexOfLeftPart($equilibriumIndex)
+    {
+        $lowerIndices = 0;
+
+        for ($index = 0; $index < $equilibriumIndex; ++$index) {
+            $lowerIndices = $lowerIndices + $this->data[ $index ];
+        }
+
+        $higherIndices = 0;
+        $dataSize = count($this->data);
+
+        for ($index = $equilibriumIndex + 1; $index < $dataSize; ++$index) {
+            $higherIndices = $higherIndices + $this->data[ $index ];
+        }
+
+        return $lowerIndices === $higherIndices;
+    }
+
+    /**
+     * @return int
+     */
+    private function noIndices()
+    {
+        return -1;
+    }
 }
--- tests/unit/EquilibriumTest.php
+++ tests/unit/EquilibriumTest.php
@@ -1,24 +1,42 @@
 <?php
 
+use App\Equilibrium;
+
 class EquilibriumTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider provider
      * @test
-     * @param array $cases
+     * @param array $data
+     * @param array $indices
      */
-    public function it_returns_index(array $cases)
+    public function it_returns_index(array $data, array $indices)
     {
-        $equilibrium = new Equilibrium;
+        $equilibrium = new Equilibrium($data);
 
-        $this->assertEquals($cases[ 'indices' ], $equilibrium->getIndices($cases[ 'array' ]));
+        $this->assertSame($indices, $equilibrium->getIndices($data));
+    }
+
+    /**
+     * @dataProvider provider
+     * @test
+     * @param array $data
+     * @param array $indices
+     */
+    public function it_checks_if_an_element_is_equilibrium(array $data, array $indices)
+    {
+        $equilibrium = new Equilibrium($data);
+
+        $this->assertTrue($equilibrium->isEquilibriumIndexOfLeftPart(1));
+//        $this->assertTrue($equilibrium->isEquilibriumIndex(3));
+//        $this->assertTrue($equilibrium->isEquilibriumIndex(7));
     }
 
     public function provider()
     {
         return [
             'original' => [
-                'array'   => [-1, 3, -4, 5, 1, -6, 2, 1],
+                'data'    => [-1, 3, -4, 5, 1, -6, 2, 1],
                 'indices' => [1, 3, 7],
             ],
         ];
