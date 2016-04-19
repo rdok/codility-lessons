fb3d5092adf2ea00568e8bdbf9fa62e4d35a3e55
Rizart Dokollari <r.dokollari@gmail.com>
2016-04-19 09:28:56 +0000

    Find longest binary gap. Resolve #1.

--- 01-iterations/app/Utilities/IntegersUtility.php
+++ 01-iterations/app/Utilities/IntegersUtility.php
@@ -6,5 +6,37 @@
  */
 class IntegersUtility
 {
+    /**
+     * @param $binary string
+     * @return int
+     */
+    public function getLongestZeroSequence($binary)
+    {
+        $binary = str_split($binary);
 
+        // default value for longest sequence is zero
+        $longestSequence = 0;
+        $currentSequence = $longestSequence;
+
+        // loop through each bit
+        foreach ($binary as $bit) {
+
+            // if current bit is zero
+            if ($bit === '0') {
+                //    add to current sequence plus one
+                $currentSequence++;
+            } else {
+                // if current bit is one
+                //   check if the new sequence is greater than the longest sequence found
+                if ($currentSequence > $longestSequence) {
+                    //      if so replace it with the last one
+                    $longestSequence = $currentSequence;
+                }
+
+                $currentSequence = 0;
+            }
+        }
+
+        return $longestSequence;
+    }
 }
---
+++ 01-iterations/tests/IntegersUtilityTest.php
@@ -0,0 +1,17 @@
+<?php
+use App\Utilities\IntegersUtility;
+
+/**
+ * @author Rizart Dokollar <r.dokollari@gmail.com
+ * @since 4/19/16
+ */
+class IntegersUtilityTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_returns_longest_sequence_of_zero()
+    {
+        $integersUtility = new IntegersUtility();
+
+        $this->assertSame(5, $integersUtility->getLongestZeroSequence(decbin(1041)));
+    }
+}
